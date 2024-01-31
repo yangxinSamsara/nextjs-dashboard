ee5c73a919906ae2be2aac09c094da093c6958cd
yangxin <yangxin@xinpianchang.com>
2024-01-31 19:01:36 +0800

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lunasima } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lunasima({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
